2f650fe0416466a4acb4026e22d69cf64154cd33
InWamos <mykyta.pidgornyi@gmail.com>
2024-05-11 15:49:40 +0300

    add modules to the courses

--- educationalPlatform/courses/models.py
+++ educationalPlatform/courses/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from .fields import OrderField
 
 # Create your models here.
 
@@ -40,9 +41,13 @@ class Module(models.Model):
     course = models.ForeignKey(Course, related_name="modules", on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=["course"])
+
+    class Meta:
+        ordering = ["order"]
 
     def __str__(self):
-        return self.title
+        return f"{self.order}. {self.title}"
 
 
 class Content(models.Model):
@@ -56,6 +61,10 @@ class Content(models.Model):
     )
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey("content_type", "object_id")
+    order = OrderField(blank=True, for_fields=["module"])
+
+    class Meta:
+        ordering = ["order"]
 
 
 class ItemBase(models.Model):
