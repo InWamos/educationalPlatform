c3b8cf6d25d781f26e1defc66f3208ea637301bc
InWamos <mykyta.pidgornyi@gmail.com>
2024-05-12 15:30:33 +0300

    chore: Add course detail and list templates

--- educationalPlatform/courses/views.py
+++ educationalPlatform/courses/views.py
@@ -1,17 +1,41 @@
 from django.shortcuts import redirect, get_object_or_404, render
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic.base import TemplateResponseMixin, View
+from django.views.generic.detail import DetailView
 from django.forms.models import modelform_factory
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.apps import apps
 from django.urls import reverse_lazy
+from django.db.models import Count
 from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
-from .models import Course
+from .models import Course, Subject
 from .forms import ModuleFormSet
 from .models import Module, Content
 
 # Create your views here.
+
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = "courses/course/detail.html"
+
+
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = "courses/course/list.html"
+
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(total_courses=Count("courses"))
+        courses = Course.objects.annotate(total_modules=Count("modules"))
+
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+
+        return self.render_to_response(
+            {"subjects": subjects, "subject": subject, "courses": courses}
+        )
 
 
 class ModuleOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
