6c0791c5c3505d733db507c3e736b089a19bee9d
InWamos <mykyta.pidgornyi@gmail.com>
2024-05-12 18:43:56 +0300

    views cache

--- educationalPlatform/students/urls.py
+++ educationalPlatform/students/urls.py
@@ -1,3 +1,4 @@
+from django.views.decorators.cache import cache_page
 from django.urls import path
 from . import views
 
@@ -15,12 +16,12 @@ urlpatterns = [
     path("courses/", views.StudentCourseListView.as_view(), name="student_course_list"),
     path(
         "course/<pk>/",
-        views.StudentCourseDetailView.as_view(),
+        cache_page(60 * 15)(views.StudentCourseDetailView.as_view()),
         name="student_course_detail",
     ),
     path(
         "course/<pk>/<module_id>/",
-        views.StudentCourseDetailView.as_view(),
+        cache_page(60 * 15)(views.StudentCourseDetailView.as_view()),
         name="student_course_detail_module",
     ),
 ]
