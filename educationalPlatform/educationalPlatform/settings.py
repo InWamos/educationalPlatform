5323a273368cdabd1aee676c3c046bbb85265fca
InWamos <mykyta.pidgornyi@gmail.com>
2024-05-12 17:20:05 +0300

    add cache to the settings

--- educationalPlatform/educationalPlatform/settings.py
+++ educationalPlatform/educationalPlatform/settings.py
@@ -55,6 +55,13 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'educationalPlatform.urls'
 
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
