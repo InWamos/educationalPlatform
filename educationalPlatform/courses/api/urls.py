711ff634103de4e04da1a6037cb73a29dca7da1a
InWamos <mykyta.pidgornyi@gmail.com>
2024-05-13 15:14:39 +0300

    Course enroll view

--- educationalPlatform/courses/api/urls.py
+++ educationalPlatform/courses/api/urls.py
@@ -6,4 +6,7 @@ app_name = "courses"
 urlpatterns = [
     path("subjects/", views.SubjectListView.as_view(), name="subject_list"),
     path("subjects/<pk>/", views.SubjectDetailView.as_view(), name="subject_detail"),
+    path(
+        "courses/<pk>/enroll/", views.CourseEnrollView.as_view(), name="course_enroll"
+    ),
 ]
