1f4536d85befe853dd085910c4a974a02d867625
InWamos <mykyta.pidgornyi@gmail.com>
2024-05-13 19:40:38 +0300

    Basic websockets

--- educationalPlatform/educationalPlatform/asgi.py
+++ educationalPlatform/educationalPlatform/asgi.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 
+import chat.routing
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "educationalPlatform.settings")
 
@@ -19,5 +21,6 @@ django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
+        "websocket": AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns)),
     }
 )
