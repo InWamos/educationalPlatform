9ec4702041a6999fe925c57c236049de8336cb0b
InWamos <mykyta.pidgornyi@gmail.com>
2024-05-13 15:50:08 +0300

    add router for course enrollment

--- educationalPlatform/courses/api/urls.py
+++ educationalPlatform/courses/api/urls.py
@@ -10,8 +10,5 @@ router.register("courses", views.CourseViewSet)
 urlpatterns = [
     path("subjects/", views.SubjectListView.as_view(), name="subject_list"),
     path("subjects/<pk>/", views.SubjectDetailView.as_view(), name="subject_detail"),
-    path(
-        "courses/<pk>/enroll/", views.CourseEnrollView.as_view(), name="course_enroll"
-    ),
     path("", include(router.urls)),
 ]
--- educationalPlatform/courses/api/views.py
+++ educationalPlatform/courses/api/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
 from rest_framework import generics, viewsets
 from courses.models import Subject, Course
 from courses.api.serializers import SubjectSerializer, CourseSerializer
@@ -18,17 +19,17 @@ class SubjectDetailView(generics.RetrieveAPIView):
     serializer_class = SubjectSerializer
 
 
-class CourseEnrollView(APIView):
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    def post(self, request, pk, format=None):
-        course = get_object_or_404(Course, pk=pk)
-        course.students.add(request.user)
-        return Response({"enrolled": True})
-
-
 class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
+    @action(
+        detail=True,
+        methods=["post"],
+        authentication_classes=[BasicAuthentication],
+        permission_classes=[IsAuthenticated],
+    )
+    def enroll(self, request, *args, **kwargs):
+        course = self.get_object()
+        course.students.add(request.user)
+        return Response({"enrolled": True})
